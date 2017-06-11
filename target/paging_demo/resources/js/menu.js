fcbbd0d4b6d62f9d6aea6986378f4b54bfd39945
Tham19 <nguyenvanthamtclc@gmail.com>
2017-06-12 01:07:08 +0700

    add paging using ajax loading data but not complete

--- target/paging_demo/resources/js/menu.js
+++ target/paging_demo/resources/js/menu.js
@@ -1,45 +1,61 @@
 (function (global) {
-
     var menu_parrtern = "resources/snippets/menu_parttern.jsp";
-    var allCategoriesUrl = "http://localhost:8080/categories";
-    document.addEventListener("DOMContentLoaded", function (event) {
+    var partternCategoriesUrl = "http://localhost:8080/categories/";
 
-        var insertHtml = function (selector, html){
-            var targetElem = document.querySelector(selector);
-            targetElem.innerHTML = html;
-        };
+    var insertHtml = function (selector, html) {
+        var targetElem = document.querySelector(selector);
+        targetElem.innerHTML = html;
+    };
 
-        var showLoading = function (selector) {
-            var html = "<div class = 'text-center'>";
-            html += "<img src = '/resources/images/ajax-loader.gif'></div>";
-            insertHtml(selector, html);
-        };
+    var showLoading = function (selector) {
+        var html = "<div class = 'text-center'>";
+        html += "<img src = '/resources/images/ajax-loader.gif'></div>";
+        insertHtml(selector, html);
+    };
 
-        var insertProperty = function (string, propName, propValue) {
-            var propToReplace = "{{" + propName + "}}";
-            string = string.replace(new RegExp(propToReplace, "g"), propValue);
-            return string;
-        };
+    var insertProperty = function (string, propName, propValue) {
+        var propToReplace = "{{" + propName + "}}";
+        string = string.replace(new RegExp(propToReplace, "g"), propValue);
+        return string;
+    };
 
-        // On first load, show home view
-        showLoading("#menu");
-        $.getJSON(allCategoriesUrl, function (data) {
+    var loadingData = function (currentPath) {
+        $.getJSON(currentPath, function (data) {
             console.log(data);
             $ajaxUtils.sendGetRequest(menu_parrtern, function (categories) {
-                var finalHTml="";
-                for(var i = 0; i < data.length; i++){
+                var finalHTml = "";
+                for (var i = 0; i < data.length; i++) {
                     var html = categories;
                     var image = data[i].image;
                     var name = data[i].name;
-                    html = insertProperty(html,"image",image);
-                    html = insertProperty(html,"name",name);
+                    html = insertProperty(html, "image", image);
+                    html = insertProperty(html, "name", name);
                     finalHTml += html;
                 }
-
                 insertHtml("#menu", finalHTml);
 
-            },false);
-
+            }, false);
         });
+    }
+
+    $("#paging li").click(function () {
+        showLoading("#menu");
+        var currentIndex = $(this).text();
+        var currentPath = partternCategoriesUrl.concat(currentIndex);
+        loadingData(currentPath);
+    });
+
+
+    document.addEventListener("DOMContentLoaded", function (event) {
+        var defaultIndex = partternCategoriesUrl.concat(1);
+        // On first load, show home view
+        showLoading("#menu");
+        loadingData(defaultIndex);
     });
 })(window);
+
+
+
+
+
+
--- target/paging_demo/views/menu.jsp
+++ target/paging_demo/views/menu.jsp
@@ -103,10 +103,22 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     <div class="container">
         <h2 class="tittle-w3">Gallery</h2>
         <div class="gallery-grids">
-
-            <%--try--%>
             <div id="menu"></div>
-                <%--try--%>
+
+            <div class="container">
+                <ul id="paging" class="pagination">
+                    <li><a href="#">&lt;&lt;</a></li>
+                    <li><a href="#">&lt;</a></li>
+                    <li><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">&gt;</a></li>
+                    <li><a href="#">&gt;&gt;</a></li>
+                </ul>
+            </div>
+
             <div class="clearfix"> </div>
         </div>
     </div>
